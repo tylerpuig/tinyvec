abae88a77007d0d91843e021a28fb0e0713153b3
tylerpuig <tphockey96@yahoo.com>
2025-02-18 12:46:16 -0600

    ts-jest setup

---
+++ bindings/nodejs/jest.config.ts
@@ -0,0 +1,18 @@
+/** @type {import('jest').Config} */
+const config = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/tests"],
+  testMatch: ["**/*.test.ts"],
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.json",
+      },
+    ],
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+};
+
+export default config;
